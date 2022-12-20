10df79768543577cb8e3bb916575c66dfcf0174e
aina1997 <ainapalacioscorral@gmail.com>
2022-12-20 18:03:24 +0100

    Cap4

---
+++ Capitol4/app.py
@@ -0,0 +1,76 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request
+from datetime import datetime, timezone
+import joblib
+from classes.customTransformers import CustomTransformer
+import pandas as pd
+import json
+import numpy as np
+
+
+load_dotenv()  # loads variables from .env file into environment
+
+app = Flask(__name__)
+url = os.environ.get("DATABASE_URL")  # gets variables from environment
+connection = psycopg2.connect(url)
+
+@app.before_request
+def middleware():
+    print('Hello Middleware Before Request!')
+
+@app.before_first_request
+def middleware():
+    print(request.headers.get('User-Agent'))
+    print('Hello first Middleware')
+
+
+@app.after_request 
+def after_request_func(response):
+    print("after_request!")
+    return response
+
+@app.teardown_request
+def middleware(exception):
+    print('Hello teardown_request')
+
+
+@app.post("/api/titanic")
+def get_prediction():
+
+    ## PassengerId,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
+    ## 892,3,"Kelly, Mr. James",male,34.5,0,0,330911,7.8292,,Q
+    ## values = [None,None,'','male',None,None,None,None,None,None, None]
+    
+    data = request.get_json()
+
+    columns = ['PassengerId','Pclass','Name','Sex','Age','SibSp','Parch','Ticket','Fare','Cabin','Embarked']
+    values = []
+    for column in columns:
+        value = data[column] if column in data else None
+        if column in ['Name', 'Sex'] and value == None:
+            print(value)
+            return {'message': "You must add a name and a sex"}, 404
+        values.append(value)
+
+    try:
+        df= pd.DataFrame(np.array([values]), columns=columns)
+        result = []
+
+        filename = './best_model.pkl'
+        
+        pipeline_loaded = joblib.load('./best_model.pkl')
+        result = pipeline_loaded.predict(df)
+        return {'message': True, "result": result.tolist()},201
+
+
+    except:
+      # otherwise the userId does not exist
+        return {
+         'message': "Error"
+        }, 404
+
+
+    
+
